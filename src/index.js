3a042f1d1c36882c4114ce69c3c0597573b64342
Endzhe Galieva <endzhe91@gmail.com>
2021-07-03 21:58:09 +0300

    create new project

--- src/index.js
+++ src/index.js
@@ -1,17 +1,25 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDom from 'react-dom';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+// stateless functional component
+// always return JSX
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+function Greeting() {
+  return (
+    <div class=''>
+      <h3>hello people</h3>
+      <ul>
+        <li>
+          <a href="#">hello world</a>
+        </li>
+      </ul>
+    </div>
+  );
+}
+
+// const Greeting = () => {
+//   return React.createElement('div', {}, React.createElement('h1', {}, 'hello world')
+//   );
+// };
+
+ReactDom.render(<Greeting />, document.getElementById('root'));
