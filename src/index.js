696d05c24b044ea1032826e487e8dbf2ee3f575e
Endzhe Galieva <endzhe91@gmail.com>
2021-07-04 11:51:35 +0300

    JSX Rules

--- src/index.js
+++ src/index.js
@@ -1,25 +1,12 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 
-// stateless functional component
-// always return JSX
-
 function Greeting() {
-  return (
-    <div class=''>
-      <h3>hello people</h3>
-      <ul>
-        <li>
-          <a href="#">hello world</a>
-        </li>
-      </ul>
+  return(
+    <div>
+      <h1>hello world</h1>
     </div>
   );
 }
-
-// const Greeting = () => {
-//   return React.createElement('div', {}, React.createElement('h1', {}, 'hello world')
-//   );
-// };
 
 ReactDom.render(<Greeting />, document.getElementById('root'));
