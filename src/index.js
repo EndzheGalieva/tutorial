d3780295986a204755bbaf014ee8319bcb2b3d2b
Endzhe Galieva <endzhe91@gmail.com>
2021-07-04 15:23:09 +0300

    Mini Book Project

--- src/index.js
+++ src/index.js
@@ -1,18 +1,34 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 
-function Greeting() {
+function BookList() {
   return (
-    <div>
-      <Person />
-      <Message />
-    </div>
+  <section>
+    <Book />
+    <Book />
+    <Book />
+    <Book />
+    <Book />
+    <Book />
+    <Book />
+  </section>
   );
 }
 
-const Person = () =>  <h2>john doe</h2>;
-const Message = () => {
-  return <p>this is my message</p>;
+const Book = () => {
+  return (
+    <article>
+      <Image></Image>
+      <Title />
+      <Author />
+    </article>
+  );
 };
 
-ReactDom.render(<Greeting />, document.getElementById('root'));
+
+
+const Image = () => <img src="https://m.media-amazon.com/images/I/81eB+7+CkUL._AC_UY218_.jpg" alt=""/>
+
+const Title = () => <h1>I Love You to the Moon and Back</h1>;
+const Author = () => <h4>Amelia Hepworth</h4>
+ReactDom.render(<BookList />, document.getElementById('root'));
