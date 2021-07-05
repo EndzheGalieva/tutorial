310f8ef7342470a7d1ce284ab8aece2a6ec2281d
Endzhe Galieva <endzhe91@gmail.com>
2021-07-05 16:49:32 +0300

    Simple List

--- src/index.js
+++ src/index.js
@@ -1,45 +1,39 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 
+// CSS
 import './index.css';
-// setup vars
-const firstBook = {
-  img: "https://m.media-amazon.com/images/I/81eB+7+CkUL._AC_UY218_.jpg",
+const books = [
+ {
+  img:
+ "https://m.media-amazon.com/images/I/81eB+7+CkUL._AC_UY218_.jpg",
   title: 'I Love You to the Moon and Back',
   author: 'Amelia Hepworth',
-};
-const secondBook = {
-  img: "https://images-na.ssl-images-amazon.com/images/I/510g8NLbpNL._SX218_BO1,204,203,200_QL40_FMwebp_.jpg",
-  title: 'Our Class is a Famil',
+  },
+  {
+  img:
+    "https://images-na.ssl-images-amazon.com/images/I/510g8NLbpNL._SX218_BO1,204,203,200_QL40_FMwebp_.jpg",
+  title: 'Our Class is a Family',
   author: 'Shannon Olsen ',
-};
-
+  },
+];
+const names = ['john', 'peter', 'susan'];
+const newNames = names.map((name) => {
+  return <h1>{name}</h1>
+});
+console.log(newNames);
 function BookList() {
-  return (
-  <section className='booklist'>
-    <Book img={firstBook.img}
-          title={firstBook.title}
-          author={firstBook.author}
-    >
-     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore cum animi voluptas temporibus quo delectus pariatur, hic voluptatum accusantium illo.</p>
-    </Book>
-    <Book img={secondBook.img}
-          title={secondBook.title}
-          author={secondBook.author}
-    />
-  </section>
-  );
+  return <section className='booklist'>{newNames}</section>;
 }
 
 const Book = (props) => {
-  const {img, title, author, children } = props;
-  console.log(props);
+  const { img, title, author } = props;
+
   return (
     <article className='book'>
       <img src={img} alt=""/>
       <h1>{title}</h1>
       <h4>{author}</h4>
-      {props.children}
     </article>
   );
 };
