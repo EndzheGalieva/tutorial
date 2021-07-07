6fa71e0b2d1e1d45766f28b4398f32b7e70c18ee
Endzhe Galieva <endzhe91@gmail.com>
2021-07-07 15:03:53 +0300

    Import And Export Statements

---
+++ src/books.js
@@ -0,0 +1,22 @@
+export const data = [
+  { id:1,
+    img:
+      "https://m.media-amazon.com/images/I/81eB+7+CkUL._AC_UY218_.jpg",
+    title: 'I Love You to the Moon and Back',
+    author: 'Amelia Hepworth',
+  },
+  {
+    id:2,
+    img:
+      "https://m.media-amazon.com/images/I/71aLultW5EL._AC_UY218_.jpg",
+    title: 'Our Class is a Family',
+    author: 'Shannon Olsen ',
+  },
+  {
+    id: 3,
+    img:
+      "https://m.media-amazon.com/images/I/71gGPRRlyTL._AC_UY218_.jpg",
+    title: 'The Vanishing Half: A Novel',
+    author: 'Brit Bennett ',
+  },
+];
